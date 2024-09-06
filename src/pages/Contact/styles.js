5b0b3d367ab884c6773ec70c83bbd13632c8ba57
Hugo Ishimoto <hugoishimoto01@gmail.com>
2024-09-06 18:07:35 -0300

    ajusted contact page responsivity

--- src/pages/Contact/styles.js
+++ src/pages/Contact/styles.js
@@ -21,9 +21,25 @@ export const Container = styled.div`
   }
 
   @media (min-width: 768px) {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    
     margin: 2.4rem 4rem;
 
+    header {
+      width: 100%;
+      max-width: 108rem;
+    }
+
+    h1, p, .contacts-wrapper {
+      max-width: 108rem;
+    }
+
     h1 {
+      display: block;
+      width: 100%;
+
       font-size: 4rem;
       margin-top: 8rem;
     }
