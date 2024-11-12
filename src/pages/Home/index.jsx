7768ef52e77f58bf28ce96f254aca13e832ba320
Hugo Ishimoto <hugoishimoto01@gmail.com>
2024-11-11 22:25:08 -0300

    fixed pinned projects url

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -20,7 +20,7 @@ const pinnedProjects = [
     "title": "Aria Discord Bot",
     "description": "Aria Futaba is a fictional character from the animated series Aikatsu Stars! She is the main theme for the bot!",
     "image": "https://camo.githubusercontent.com/c88ddae516a261b5ff3cca90d160730d7c282529605b9d70c5bb48ac4b238f88/68747470733a2f2f7374617469632e77696b69612e6e6f636f6f6b69652e6e65742f61696b617473757374617273363836342f696d616765732f662f66302f415338323132322e706e672f7265766973696f6e2f6c61746573743f63623d3230313731313136313735303534",
-    "link": "https://github.com/ishugodev"
+    "link": "https://github.com/thiagokrugel"
   }
 ]
 
